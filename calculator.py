8b275141c07bbe2b8ad869e53e9937b5c87cac4d
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-18 14:28:10 -0700

    very basic 2 input calculation functions

--- calculator.py
+++ calculator.py
@@ -1,7 +1,44 @@
-def format_name(f_name, l_name):
-    name = f"{f_name} {l_name}"
-    return name.title()
+from art import logo
+
+print(logo)
 
 
-formatted_output = format_name("james", "BOND")
-print(formatted_output)
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+
+num2 = int(input("What's the second number?: "))
+
+
+def calculator(n1, n2, operation):
+    answer = operations[operation](n1, n2)
+    print(f"{n1} {operation} {n2} = {answer}")
+
+
+calculator(num1, num2, operation_symbol)
