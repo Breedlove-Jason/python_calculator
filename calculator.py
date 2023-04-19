ff2ccbfae6b0051cbafd131f02a432c3c44eaec9
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-19 13:02:49 -0700

    added recursion to restart calculator

--- calculator.py
+++ calculator.py
@@ -26,28 +26,32 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
 
-for symbol in operations:
-    print(symbol)
+def calc():
+    num1 = float(input("What's the first number?: "))
+
+    for symbol in operations:
+        print(symbol)
+
+    def calculator(n1, n2, operation):
+        answer = operations[operation](n1, n2)
+        return answer
+
+    continue_calculation = True
+
+    while continue_calculation:
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = float(input("What's the next number?: "))
+        result = calculator(num1, num2, operation_symbol)
+        print(f"{num1} {operation_symbol} {num2} = {result}")
+
+        user_input = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+
+        if user_input == "y":
+            num1 = result
+        else:
+            continue_calculation = False
+            calc()
 
 
-def calculator(n1, n2, operation):
-    answer = operations[operation](n1, n2)
-    return answer
-
-
-continue_calculation = True
-
-while continue_calculation:
-    operation_symbol = input("Pick an operation from the line above: ")
-    num2 = int(input("What's the next number?: "))
-    result = calculator(num1, num2, operation_symbol)
-    print(f"{num1} {operation_symbol} {num2} = {result}")
-
-    user_input = input(f"Type 'y' to continue calculating with {result}, or type 'n' to quit: ")
-
-    if user_input == "y":
-        num1 = result
-    else:
-        continue_calculation = False
+calc()
