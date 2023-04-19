4c8ffb545fe5b4cd45b585a126db3ef0b7fde233
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-19 11:46:23 -0700

    added while loop for multiple calculations

--- calculator.py
+++ calculator.py
@@ -31,14 +31,23 @@ num1 = int(input("What's the first number?: "))
 for symbol in operations:
     print(symbol)
 
-operation_symbol = input("Pick an operation from the line above: ")
-
-num2 = int(input("What's the second number?: "))
-
 
 def calculator(n1, n2, operation):
     answer = operations[operation](n1, n2)
-    print(f"{n1} {operation} {n2} = {answer}")
+    return answer
 
 
-calculator(num1, num2, operation_symbol)
+continue_calculation = True
+
+while continue_calculation:
+    operation_symbol = input("Pick an operation from the line above: ")
+    num2 = int(input("What's the next number?: "))
+    result = calculator(num1, num2, operation_symbol)
+    print(f"{num1} {operation_symbol} {num2} = {result}")
+
+    user_input = input(f"Type 'y' to continue calculating with {result}, or type 'n' to quit: ")
+
+    if user_input == "y":
+        num1 = result
+    else:
+        continue_calculation = False
